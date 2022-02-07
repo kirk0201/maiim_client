a21d700b6cc4589e2cbce85f2962f8b1ee48a38a
JaeHyen Kim <mine6546@gmail.com>
2022-02-07 18:42:33 +0900

    Setting/ nextJS와 Style-components 초기 세팅을 해라

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import { GlobalStyle } from "../styles/global-styles";
+import type { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
